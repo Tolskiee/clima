7829cdf6703a695835438ded3df4730db5f610bd
Val Jeremy Tolosa <vjeremytolosa@gmail.com>
2022-08-29 10:03:54 +0800

    create get location button and use permission handler package

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  void checkPermission(Permission permission) async {
+    var status = await permission.status;
+    if (status.isDenied) {
+      await permission.request();
+    }
+  }
+
+  void getLocation() async {
+    checkPermission(Permission.location);
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            getLocation();
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
